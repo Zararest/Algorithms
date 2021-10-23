0b314659516a0f3247d2d7bb6c0f3a75085c3636
UwU <iishumakov@gmail.com>
2021-10-23 16:11:25 +0300

    New version of 2_7

--- HW/main.cpp
+++ HW/main.cpp
@@ -16,52 +16,81 @@ struct Line{
 
     FILE* file;
     int cur_arr_position;
-    int cur_file_position;
     char cur_elem[MAXLEN];
+
+    void init(FILE* new_file){
+
+        file = new_file;
+        cur_arr_position = -1;
+    }
 
     void set_zero(){
         
-        cur_arr_position = 0;
+        cur_arr_position = -1;
     }
 
     void go_to_beggining(){
 
         fseek(file, SEEK_SET, 0);
-        cur_arr_position = 0;
-        cur_file_position = 0;
+        cur_arr_position = -1;
     }
 
     void add_elem(const char* new_str){
 
         fprintf(file, "%s\n", new_str);
-        cur_file_position++;
         cur_arr_position++;
     }
 
     const char* get_elem(int position){
+
+        if (cur_arr_position == -1){
+
+            if (fscanf(file, "%s", cur_elem) != EOF){
+
+                cur_arr_position++;
+            } else{
+
+                printf("End of line\n");
+                return nullptr;
+            }
+        }
 
         if (position == cur_arr_position){
 
             return cur_elem;
         }
 
-        if (position > cur_arr_position){
+        if (position - 1 == cur_arr_position){
 
-            fseek(file, SEEK_CUR, position - cur_arr_position - 1);
-            cur_file_position += position - cur_arr_position - 1;
-            cur_arr_position += position - cur_arr_position - 1;
+            if (fscanf(file, "%s", cur_elem) != EOF){
 
-            fscanf(file, "%s", cur_elem);
-            cur_file_position++;
-            cur_arr_position++;
+                cur_arr_position++;
+                return cur_elem;
+            } else{
+
+                printf("End of line!!!!\n");
+                return nullptr;
+            }
         }
 
-        printf("attempt to go back\n");
+        printf("incorrect line move\n");
         return nullptr;
     } 
+
+    void print(FILE* output){
+
+        int old_position = ftell(file);
+        char tmp_str[MAXLEN];
+
+        fseek(file, SEEK_SET, 0);
+        while (fscanf(file, "%s", tmp_str) != EOF){
+
+            fprintf(output, "%s\n", tmp_str);
+        }
+        
+        fseek(file, SEEK_SET, old_position);
+    }
 };
-
-
 
 
 int compare_string(const void* fir_str, const void* sec_str){
@@ -69,19 +98,15 @@ int compare_string(const void* fir_str, const void* sec_str){
     return strcmp(*(const char**)fir_str, *(const char**)sec_str);
 }
 
-void put_buffer_to(Buffer& buf, FILE* file){
-
-   qsort(buf.data, buf.cur_size, sizeof(char*), compare_string);
+void put_buffer_to(Buffer& buf, Line& file){
 
    for (int i = 0; i < buf.cur_size; i++){
 
-       fprintf(file, "%s\n", buf.data[i]);
-       printf("buf |%s|\n",  buf.data[i]);
-   }
-   printf("-----\n\n");
+       file.add_elem(buf.data[i]);
+    }
 }
 
-int buffer_sort(FILE* input_file, FILE* first_line, FILE* second_line){
+int buffer_sort(FILE* input_file, Line& first_line, Line& second_line){
 
     int itter = 0, cur_line = 1, number_of_str = 0;
     bool end_of_file = false;
@@ -109,10 +134,12 @@ int buffer_sort(FILE* input_file, FILE* first_line, FILE* second_line){
 
                 if (cur_line == 1){
 
+                    qsort(buffer.data, buffer.cur_size, sizeof(char*), compare_string);
                     put_buffer_to(buffer, first_line);
                     cur_line = 2;
                 } else{
 
+                    qsort(buffer.data, buffer.cur_size, sizeof(char*), compare_string);
                     put_buffer_to(buffer, second_line);
                     cur_line = 1;
                 }
@@ -125,10 +152,12 @@ int buffer_sort(FILE* input_file, FILE* first_line, FILE* second_line){
 
             if (cur_line == 1){
 
+                qsort(buffer.data, buffer.cur_size, sizeof(char*), compare_string);
                 put_buffer_to(buffer, first_line);
                 cur_line = 2;
             } else{
 
+                qsort(buffer.data, buffer.cur_size, sizeof(char*), compare_string);
                 put_buffer_to(buffer, second_line);
                 cur_line = 1;
             }
@@ -138,31 +167,21 @@ int buffer_sort(FILE* input_file, FILE* first_line, FILE* second_line){
     return number_of_str;
 }
 
-void merge(FILE* first_line, FILE* second_line, FILE* output, int first_line_len, int second_line_len){
+void merge(Line& first_line, Line& second_line, FILE* output, int first_line_len, int second_line_len){
 
-    int fir_itter = 1, sec_itter = 1;
-    int number_of_overflow = 0;
-    char fir_line_str[MAXLEN], sec_line_str[MAXLEN];
-    
-    if (first_line_len > 0){
-        fscanf(first_line, "%s", fir_line_str);
-    }
-    
-    if (second_line_len > 0){
-        fscanf(second_line , "%s", sec_line_str);
-    }
-    
+    int fir_itter = 0, sec_itter = 0;
+    first_line.set_zero();
+    second_line.set_zero();
+
     while ((fir_itter < first_line_len) && (sec_itter < second_line_len)){
         
-        if (strcmp(fir_line_str, sec_line_str) < 0){
+        if (strcmp(first_line.get_elem(fir_itter), second_line.get_elem(sec_itter)) < 0){
             
-            fprintf(output, "%s\n", fir_line_str);
-            if (fscanf(first_line, "%s", fir_line_str) == EOF) number_of_overflow++;
+            fprintf(output, "%s\n", first_line.get_elem(fir_itter)); 
             fir_itter++;
         } else{
 
-            fprintf(output, "%s\n", sec_line_str);
-            if (fscanf(second_line, "%s", sec_line_str) == EOF) number_of_overflow++;//кажись на одну итерацию обосрался 
+            fprintf(output, "%s\n", second_line.get_elem(sec_itter));
             sec_itter++;
         }
     }
@@ -171,44 +190,43 @@ void merge(FILE* first_line, FILE* second_line, FILE* output, int first_line_len
 
         while (fir_itter < first_line_len){
             
-            fprintf(output, "%s\n", fir_line_str);
-            fir_itter++;
-            if (fscanf(first_line, "%s", fir_line_str) == EOF) number_of_overflow++;
+            fprintf(output, "%s\n", first_line.get_elem(fir_itter));
+            fir_itter++; 
         }
     } else{
 
         while (sec_itter < second_line_len){
 
-            fprintf(output, "%s\n", sec_line_str);
-            sec_itter++;
-            if (fscanf(second_line, "%s", sec_line_str) == EOF) number_of_overflow++;
+            fprintf(output, "%s\n", second_line.get_elem(sec_itter));
+            sec_itter++; 
         }
     }   
-
-    printf("overflow number  %i\n", number_of_overflow);
 }
 
-int put_str_to(FILE* input, FILE* line, int cur_series_len){ //надо переделать без eof
+int put_str_to(FILE* input, Line& line, int cur_series_len){
 
     char cur_str[MAXLEN];
     int i = 0;
 
+    line.set_zero();
+
     for (i = 0; i < cur_series_len; i++){
 
-        assert(fscanf(input, "%s", cur_str) != EOF);
-        fprintf(line, "%s\n", cur_str);
+        fscanf(input, "%s", cur_str);
+        line.add_elem(cur_str);
     }
 
     return i;
 }
 
-void split_into_lines(FILE* first_line, FILE* second_line, FILE* output, int cur_series_len, int number_of_str){
+void split_into_lines(Line& first_line, Line& second_line, FILE* output, int cur_series_len, int number_of_str){
 
     int cur_line = 1;
     int number_of_chunks = 0, small_chunk = 0;
 
-    fseek(first_line, SEEK_SET, 0);
-    fseek(second_line, SEEK_SET, 0);
+    first_line.go_to_beggining();
+    second_line.go_to_beggining();
+
     fseek(output, SEEK_SET, 0);
 
     number_of_chunks = number_of_str / cur_series_len;
@@ -239,84 +257,68 @@ void split_into_lines(FILE* first_line, FILE* second_line, FILE* output, int cur
             put_str_to(output, second_line, small_chunk);
         }
     }
-
-    /*while (number_of_splited_strs < number_of_str){
-
-        if (cur_line == 1){
-
-            first_line_len += put_str_to(output, first_line, cur_series_len);
-            cur_line = 2;
-        } else{
-
-            second_line_len += put_str_to(output, second_line, cur_series_len);
-            cur_line = 1;
-        }
-
-        number_of_splited_strs += cur_series_len;
-    }*/
 }
 
-void merge_sort(FILE* first_line, FILE* second_line, FILE* output, int number_of_str){
+void merge_sort(Line& first_line, Line& second_line, FILE* output, int number_of_str){
 
     int cur_series_len = BUFFER_SIZE;
-    int first_line_len, second_line_len, number_of_chunks = 0, small_chunk = 0, elements_in_first = 0, elements_in_second = 0;
+    int first_line_len, second_line_len, number_of_chunks = number_of_str / cur_series_len, small_chunk = 0, elements_in_first = 0, elements_in_second = 0;
     
-    while (cur_series_len < number_of_str){
+    while (cur_series_len <= 2 * number_of_str){
 
         number_of_chunks = number_of_str / cur_series_len;
         small_chunk = number_of_str % cur_series_len;
-        printf("size of chunk = %i number of chunks = %i small = %i\n", cur_series_len, number_of_chunks, small_chunk);
 
-        fseek(first_line, SEEK_SET, 0);
-        fseek(second_line, SEEK_SET, 0);
+        first_line.go_to_beggining();
+        second_line.go_to_beggining();
         fseek(output, SEEK_SET, 0);
 
-        while (number_of_chunks >= 0){//тут надо условие поменять 
+        while (number_of_chunks >= 0){ 
             
             if (number_of_chunks >= 2){
 
                 merge(first_line, second_line, output, cur_series_len, cur_series_len);
-                elements_in_first += cur_series_len;
-                elements_in_second += cur_series_len;
                 number_of_chunks -= 2;
             } else{
                 
                 if (number_of_chunks == 1){
 
                     merge(first_line, second_line, output, cur_series_len, small_chunk);
-                    elements_in_first += cur_series_len;
-                    elements_in_second += small_chunk; 
+
+                    number_of_chunks -= 2;
                 } else{
 
                     merge(first_line, second_line, output, small_chunk, 0);
-                    elements_in_first += small_chunk;
-                }
-
-                number_of_chunks--;
+                    number_of_chunks--;
+                }                
             }
         }
         
         cur_series_len *= 2;
-        exit(0);
-        if (cur_series_len < number_of_str){
+
+        if (number_of_chunks < number_of_str){
 
             split_into_lines(first_line, second_line, output, cur_series_len, number_of_str);
         }   
     }
 }
 
-
 int main(){
 
     FILE* input_file = fopen("../bin/input.txt", "r");
     
-    FILE* first_line = fopen("../bin/first_line.txt", "w+");
-    FILE* second_line = fopen("../bin/second_line.txt", "w+");
-    FILE* output_file = fopen("../bin/output.txt", "w+");
+    FILE* first_line_file = fopen("../bin/first_line.txt", "w+t");
+    FILE* second_line_file = fopen("../bin/second_line.txt", "w+t");
+    FILE* output_file = fopen("../bin/output.txt", "w+t");
 
-    assert(first_line != nullptr);
-    assert(second_line != nullptr);
+    assert(first_line_file != nullptr);
+    assert(second_line_file != nullptr);
     assert(output_file != nullptr);
+
+    Line first_line, second_line;
+
+    first_line.init(first_line_file);
+    second_line.init(second_line_file);
     
     int number_of_lines = buffer_sort(input_file, first_line, second_line);        
 
@@ -325,11 +327,9 @@ int main(){
         merge_sort(first_line, second_line, output_file, number_of_lines);
     } else{
 
-        printf("sorted with buffer\n");
+        first_line.print(output_file);
     }
     
-    fclose(first_line);
-    fclose(second_line);
     fclose(output_file);
 }
 
