421140105a3b9bae65e5d683aa58316d45e8aa95
UwU <iishumakov@gmail.com>
2022-04-01 16:50:11 +0300

    TMP

--- HW/main.cpp
+++ HW/main.cpp
@@ -2,75 +2,197 @@
 #include <string>
 #include <iostream>
 #include <vector>
-#include <sstream>
-#define ALFAB_SIZE 26
 
-class StrSet{
+struct Life{
 
-    int leters[ALFAB_SIZE] = {};//
-    int sum = 0;
+    int X = 0;
+    int Y = 0;
 
-public:
+    Life(){}
 
-    void get_string(std::string& string){
+    Life(int new_x, int new_y){
 
-        for (auto it : string){
-
-            leters[it - 'A']++;
-        }
-
-        sum = string.size();
+        X = new_x;
+        Y = new_y;
     }
 
-    void set_zero(){
+    bool operator <(const Life& rhs) const{
 
-        for (int i = 0; i < ALFAB_SIZE; i++){
+        if (X == rhs.X) return Y < rhs.Y;
 
-            leters[i] = 0;
-        }
-    }
-
-    void print() const{
-
-        for (int i = 0; i < ALFAB_SIZE; i++){
-
-            std::cout << leters[i];
-        }
-
-        std::cout << std::endl;
-    }
-
-    bool operator <(const StrSet& rhs) const{//
-
-        int pos = 0;
-        
-        while (pos < ALFAB_SIZE && leters[pos] == rhs.leters[pos]){
-
-            pos++;
-        }
-
-        return leters[pos] < rhs.leters[pos];
+        return X < rhs.X;
     }
 };
 
-int main(){
+class Game{
 
-    int N = 0;
-    std::cin >> N;
+    int field_size_X = 0;       // x < field_size_X
+    int field_size_Y = 0;
+    std::set<Life> field;
+    std::vector<Life> about_to_die;//тут нужен итератор 
+    std::vector<Life> new_life;
 
-    std::set<StrSet> sets;
-    StrSet cur_set;
-    std::string cur_string;
-    std::ios::sync_with_stdio(false);
+    int get_num_of_neighb(int X, int Y);
+    void try_to_resurect(int X, int Y);
 
-    for (int i = 0; i < N; i++){
+public:
 
-        cur_set.set_zero();
-        std::cin >> cur_string;
-        
-        cur_set.get_string(cur_string);
-        sets.insert(cur_set);
+    Game(int N, int M);
+    void fill_field(int num_of_lives);
+    void tact();
+    void print();
+};
+
+int Game::get_num_of_neighb(int X, int Y){
+
+    Life tmp_life;
+    int result = 0;
+    tmp_life.X = X - 1;
+
+    tmp_life.Y = Y - 1;
+    auto line = field.lower_bound(tmp_life);
+
+    while (line->X > 0 && line->X <= X + 1 && line->X < field_size_X){
+
+        result++;
+        tmp_life.X++;
+        line = field.lower_bound(tmp_life);
     }
 
-    std::cout << sets.size() << std::endl;
+    tmp_life.X = X - 1;
+    tmp_life.Y = Y;
+    line = field.lower_bound(tmp_life);
+
+    while (line->X > 0 && line->X <= X + 1 && line->X < field_size_X){
+
+        result++;
+        tmp_life.X++;
+        line = field.lower_bound(tmp_life);
+    }
+
+    tmp_life.X = X - 1;
+    tmp_life.Y = Y + 1;
+    line = field.lower_bound(tmp_life);
+
+    while (line->X > 0 && line->X <= X + 1 && line->X < field_size_X){  ///!!!!!!!!!!!!!!!!!!!!!!!
+
+        result++;
+        tmp_life.X++;
+        line = field.lower_bound(tmp_life);
+    }
+    std::cout << "num of neighb: " << result << " [" << X << ", " << Y << "]" << std::endl; 
+    return result;
 }
+
+Game::Game(int N, int M){
+
+    field_size_X = N;
+    field_size_Y = M;
+}
+
+void Game::fill_field(int num_of_lives){
+
+    Life tmp_life;
+
+    for (int i = 0; i < num_of_lives; i++){
+
+        scanf("%i %i", &tmp_life.X, &tmp_life.Y);
+        field.insert(tmp_life);
+    }
+}
+
+void Game::try_to_resurect(int X, int Y){
+
+    int num_of_neighb = get_num_of_neighb(X, Y);
+
+    if (get_num_of_neighb(X, Y) == 3){
+
+        new_life.emplace_back(X, Y);
+    }
+}
+
+void Game::tact(){
+
+    Life tmp_life;
+    int cur_num_of_neighb = 0;
+
+    for(auto it : field){
+
+        int cur_num_of_neighb = get_num_of_neighb(it.X, it.Y) - 1; //тк мы знаем что она живая
+
+        if (cur_num_of_neighb < 2 || cur_num_of_neighb > 3){
+
+            about_to_die.push_back(it);
+        }
+
+        if (it.X + 1 < field_size_X){
+
+            try_to_resurect(it.X + 1, it.Y);
+        }
+
+        if (tmp_life.Y + 1 < field_size_Y){
+
+            for (int X = it.X - 1; X <= it.X + 1 && X < field_size_X && X > 0; X++){
+
+                try_to_resurect(X, it.Y + 1);
+            }
+        }
+    }
+
+    for (auto it : new_life){
+
+        field.insert(it);
+    }
+
+    for (auto it : about_to_die){
+
+        field.erase(it);
+    }
+}
+
+void Game::print(){
+
+    for (auto it : field){
+
+        printf("%i %i\n", it.X, it.Y);
+    }
+}
+
+int main(){
+
+    int N, M, L, K;
+    std::cin >> N >> M >> L >> K;
+
+    Game game(N, M);
+    game.fill_field(L);
+
+    for (int i = 0; i < K; i++){
+
+        game.tact();
+        std::cout << "------------" << std::endl;
+        game.print();
+    }
+}
+
+
+/*
+
+tmp_life.X = it.X - 1;
+        tmp_life.Y = it.Y + 1;
+
+        auto greater_neighb = field.lower_bound(tmp_life);
+
+        while (greater_neighb->Y == it.Y + 1 && greater_neighb->X <= it.X + 1){
+
+            cur_num_of_neighb++;
+            *(int*)&greater_neighb->num_of_neighb += 1;                  //мдааааааааааааааааааааааааааа
+            greater_neighb++;
+        } 
+
+        if (it.num_of_neighb + cur_num_of_neighb < 2 || it.num_of_neighb + cur_num_of_neighb > 3){
+
+            about_to_die.push_back(it);
+        }
+
+        *(int*)&greater_neighb->num_of_neighb = 0;
+*/
